66f2b8487cc2d5319e90e1a6610164d1cf8f5d7a
Maksim-k02 <kushnerevich.m@gmail.com>
2022-01-22 13:38:17 +0300

    test commit

---
+++ src/main/java/com/epam/brest/CourseApplication.java
@@ -0,0 +1,16 @@
+package com.epam.brest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+@SpringBootApplication
+public class CourseApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+
+}
