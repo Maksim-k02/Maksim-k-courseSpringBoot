f3e99622cb0485713c9c80c906a02be0221c5db3
Maksim-k02 <kushnerevich.m@gmail.com>
2022-02-26 12:51:51 +0300

    add swagger

--- rest-app/src/main/java/com/epam/brest/RestApplication.java
+++ rest-app/src/main/java/com/epam/brest/RestApplication.java
@@ -1,8 +1,11 @@
 package com.epam.brest;
 
 
+import com.epam.brest.rest.VersionController;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
@@ -25,15 +28,12 @@ public class RestApplication extends SpringBootServletInitializer {
 
     @Bean
     public OpenAPI openAPIConfig(){
-        return new OpenAPI().info(apiInfo());
+        return new OpenAPI()
+                .info(new Info().title("Course CODE API")
+                        .description("Course project Code System Swagger Open API")
+                        .version("v1.0.0"))
+                        .externalDocs(new ExternalDocumentation()
+                        .description("GitHub")
+                        .url("https://github.com/Maksim-k02/Maksim-k-courseSpringBoot"));
     }
-
-    public Info apiInfo(){
-        Info info = new Info();
-        info.title("Course CODE API")
-                .description("Course project Code System Swagger Open API")
-                .version("v1.0.0");
-        return info;
-    }
-
 }
